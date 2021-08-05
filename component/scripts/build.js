f02c6878134c4d4917db96fe4d960820982b7d45
R. van Veen <rvanveen@servoy.com>
2021-08-05 12:21:49 +0200

    [NG2] Fix missing translation files

--- component/scripts/build.js
+++ component/scripts/build.js
@@ -6,5 +6,6 @@ var zip = new AdmZip();
 zip.addLocalFolder("./META-INF/", "/META-INF/");
 zip.addLocalFolder("./dist/servoy/smartdocumenteditor/", "/dist/servoy/smartdocumenteditor/");
 zip.addLocalFolder("./smartdocumenteditor/", "/smartdocumenteditor/");
+zip.addLocalFolder("./smartdocumenteditor/lib/translations/", "/dist/servoy/smartdocumenteditor/assets/lib/translations/");
 
 zip.writeZip("smartdocumenteditor.zip");
